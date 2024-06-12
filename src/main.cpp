3e3571653f15c80ad6ff2902c9c9de5c3a78177d
dan pascual <danilopascual527@gmail.com>
2024-06-13 05:47:57 +0800

    ESP32 - Server of Petness Dispensing Module. Established connection with esp32 client

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <HX711_ADC.h> // weight sensor
 #include "Stepper.h"   // stepper motor
-#include <esp_now.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
 // Loadcell config/constructor
 const int HX711_dout = 32; // mcu > HX711 dout pin
@@ -12,7 +12,6 @@ const int calVal_eepromAdress = 0;
 unsigned long t = 0;
 float calibrationValue = 2921.21; // CALIBRATION
 boolean dispenseMotor1ACounter = false;
-
 // Relay config/constructor
 int motor1Relay = 25; // Relay for motor1
 
@@ -23,56 +22,45 @@ const int smallStep = 55;                                     // Small step size
 Stepper motor1 = Stepper(stepsPerRevolution, 12, 14, 27, 26); // motor1
 
 // AmountToDispense
-float amountToDispense = 0; // Initialized to 0, will be updated by ESP-NOW
+float amountToDispense = 0; // Initialized to 0, will be updated by HTTP request
+boolean dataReceived = false; // Flag to check if data is received
 
-// ESP-NOW data structure
-typedef struct struct_message
-{
-  char userName[32];
-  float amountToDispense;
-  char scheduledDate[20];
-  char scheduledTime[20];
-} struct_message;
+// Definitions for WiFi
+const char* WIFI_SSID = "Tabaranza_WIFI";
+const char* WIFI_PASSWORD = "Gabby1713";
 
-// Create a struct_message called myData
-struct_message myData;
+WebServer server(80);
 
-// Flag to indicate if new data is received
-bool newDataReceived = false;
+void handleReceive() {
+  if (server.hasArg("plain")) {
+    String body = server.arg("plain");
 
-void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
-{
-  if (len == sizeof(myData))
-  {
-    memcpy(&myData, incomingData, sizeof(myData));
-    Serial.print("Data received: ");
-    Serial.print("Amount to dispense: ");
-    Serial.println(myData.amountToDispense);
-    amountToDispense = myData.amountToDispense;
-    newDataReceived = true; // Set the flag to indicate new data received
-  }
-  else
-  {
-    Serial.println("Received data length mismatch!");
+    // Print received data to Serial
+    Serial.print("Received data: ");
+    Serial.println(body);
+
+    // Parse received data to update amountToDispense
+    int index = body.indexOf("amountToDispense");
+    if (index != -1) {
+      int startIndex = body.indexOf(":", index) + 1;
+      int endIndex = body.indexOf(",", startIndex);
+      if (endIndex == -1) endIndex = body.length();
+      String amountStr = body.substring(startIndex, endIndex);
+      amountToDispense = amountStr.toFloat();
+      Serial.print("Updated amountToDispense: ");
+      Serial.println(amountToDispense);
+      dataReceived = true; // Set the flag to true
+    }
+
+    // Respond with success message
+    server.send(200, "application/json", "{\"status\":\"success\"}");
+  } else {
+    server.send(400, "application/json", "{\"status\":\"fail\",\"message\":\"Invalid request\"}");
   }
 }
 
-void setupESPNow()
-{
-  WiFi.mode(WIFI_STA); // Initialize Wi-Fi in station mode
-  if (esp_now_init() != ESP_OK)
-  {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  Serial.println("ESP-NOW Initialized");
-  esp_now_register_recv_cb(onDataRecv);
-}
-
-void dispenseMotor1A()
-{
-  for (int i = 0; i < stepsPerRevolution; i += smallStep)
-  {
+void dispenseMotor1A() {
+  for (int i = 0; i < stepsPerRevolution; i += smallStep) {
     motor1.step(smallStep); // Smaller steps for better control
     LoadCell.update();      // Update load cell value
     if (LoadCell.getData() >= amountToDispense)
@@ -80,10 +68,8 @@ void dispenseMotor1A()
   }
 }
 
-void dispenseMotor1B()
-{
-  for (int i = 0; i < stepsPerRevolution; i += smallStep)
-  {
+void dispenseMotor1B() {
+  for (int i = 0; i < stepsPerRevolution; i += smallStep) {
     motor1.step(-smallStep); // Smaller steps for better control
     LoadCell.update();       // Update load cell value
     if (LoadCell.getData() >= amountToDispense)
@@ -91,22 +77,17 @@ void dispenseMotor1B()
   }
 }
 
-void stopDispenseMotor1()
-{
+void stopDispenseMotor1() {
   digitalWrite(motor1Relay, LOW);
 }
 
-void loadCellConfiguration()
-{
+void loadCellConfiguration() {
   unsigned long stabilizingtime = 2000; // tare precision can be improved by adding a few seconds of stabilizing time
   boolean _tare = true;                 // set this to false if you don't want tare to be performed in the next step
   LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag())
-  {
+  if (LoadCell.getTareTimeoutFlag()) {
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-  }
-  else
-  {
+  } else {
     LoadCell.setCalFactor(calibrationValue); // set calibration factor (float)
     Serial.println("Startup is complete");
   }
@@ -122,26 +103,19 @@ void loadCellConfiguration()
   Serial.print("HX711 measured settling time ms: ");
   Serial.println(LoadCell.getSettlingTime());
   Serial.println("Note that the settling time may increase significantly if you use delay() in your sketch!");
-  if (LoadCell.getSPS() < 7)
-  {
+  if (LoadCell.getSPS() < 7) {
     Serial.println("!!Sampling rate is lower than specification, check MCU>HX711 wiring and pin designations");
-  }
-  else if (LoadCell.getSPS() > 100)
-  {
+  } else if (LoadCell.getSPS() > 100) {
     Serial.println("!!Sampling rate is higher than specification, check MCU>HX711 wiring and pin designations");
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   // Relay Config
   pinMode(motor1Relay, OUTPUT);
   digitalWrite(motor1Relay, LOW);
-
-  // Initialize ESP-NOW
-  setupESPNow();
 
   // Motor Config
   motor1.setSpeed(motorSpeed);
@@ -150,38 +124,47 @@ void setup()
   Serial.println("Starting...");
   LoadCell.begin();
   loadCellConfiguration();
+
+  // Initialize WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nConnected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize server routes
+  server.on("/receive", HTTP_POST, handleReceive);
+  server.begin();
+  Serial.println("Server started");
 }
 
-void loop()
-{
+void loop() {
+  server.handleClient();
   static boolean newDataReady = 0;
   const int serialPrintInterval = 100; // increase value to slow down serial print activity
+  static unsigned long lastPrintTime = 0; // Track the last print time
 
   // Check if new data is received
-  if (newDataReceived)
-  {
-    newDataReceived = false; // Reset the flag after processing
+  if (LoadCell.update())
+    newDataReady = true;
 
-    // check for new data/start next conversion:
-    if (LoadCell.update())
-      newDataReady = true;
-
+  // Check if data has been received from client
+  if (dataReceived) {
     // get smoothed value from the dataset:
-    if (newDataReady)
-    {
-      if (millis() > t + serialPrintInterval)
-      {
-
+    if (newDataReady) {
+      if (millis() > t + serialPrintInterval) {
         float petTrayAmount = LoadCell.getData();
-        if (petTrayAmount >= amountToDispense)
-        {
+        if (petTrayAmount >= amountToDispense) {
           stopDispenseMotor1();
           Serial.println(" -- Reached the Threshold, STOP");
           Serial.print("Load_cell output val: ");
           Serial.println(petTrayAmount);
-        }
-        else if (petTrayAmount < amountToDispense && !dispenseMotor1ACounter)
-        {
+          dataReceived = false;
+        } else if (petTrayAmount < amountToDispense && !dispenseMotor1ACounter) {
           digitalWrite(motor1Relay, HIGH);
           Serial.println(" Rotating Clockwise --");
           dispenseMotor1A();
@@ -190,9 +173,7 @@ void loop()
           newDataReady = 0;
           t = millis();
           dispenseMotor1ACounter = true;
-        }
-        else if (petTrayAmount < amountToDispense && dispenseMotor1ACounter)
-        {
+        } else if (petTrayAmount < amountToDispense && dispenseMotor1ACounter) {
           Serial.println("Counter Clockwise --");
           dispenseMotor1B();
           Serial.print("Load_cell output val: ");
@@ -200,12 +181,16 @@ void loop()
           newDataReady = 0;
           t = millis();
           dispenseMotor1ACounter = false;
-        }
-        else
-        {
+        } else {
           Serial.println("Stepper Motor Not Moving Because the Threshold is Zero");
         }
       }
+    }
+  } else {
+    // Print "waiting for data" every 5 seconds
+    if (millis() - lastPrintTime >= 5000) {
+      Serial.println("Waiting for data...");
+      lastPrintTime = millis();
     }
   }
 }
